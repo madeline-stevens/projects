423db020a68e9a71ed606e1de672a3ee5f3f59e1
Madeline Stevens <stevensm1987@gmail.com>
2023-04-24 13:29:47 -0700

    lots of edits to routes but still not working

--- routes/aboutController.js
+++ routes/aboutController.js
@@ -1,27 +1,49 @@
-// 'use strict';
+'use strict';
 
-var express = require('express');
-var router = express.Router();
+// // const express = require('express')
+// // const app = express()
 
-/* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Express' });
-});
-
-module.exports = router;
+// // app.get('/about', (req, res) => {
+// //   res.send('about')
+// // })
 
 
-// var app = app || {};
 
-// (function(module) {
-//   const aboutController = {};
+// // ----
+// // var express = require('express');
+// // var router = express.Router();
 
-//   aboutController.about = () => {
-//     $('.tab-content').hide();
-//     $('#about').show();
+// // /* GET about page. */
+// // router.get('/about', function(req, res, next) {
+// //   res.render('about', { title: 'Express' });
+// // });
 
-//   };
-//   module.aboutController = aboutController;
-// })(app);
+// // module.exports = router;
+// // ---
+// const express = require('express');
+// const router = espress.Router()
+// // const app = express()
+
+
+
+// router.get ("/", (req, res) => {
+//   res.send ({ data: "here is your data"})
+// })
+
+// module.exports = router;
+
+// old working site 
+var app = app || {};
+
+(function(module) {
+  const aboutController = {};
+
+  aboutController.about = () => {
+    $('.tab-content').hide();
+    $('#about').show();
+
+  };
+  module.aboutController = aboutController;
+})(app);
 
 
--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,7 @@
 var app = app || {};
 
 page('/about', app.aboutController.about);
-// page('/scholarships', app.scholarshipController.scholarships);
-// page('/programs', app.programsController.programs);
-page('/contact', app.contactController.contact);
+// page('/contact', app.contactController.contact);
 page('/', app.homeController.home);
 
 
